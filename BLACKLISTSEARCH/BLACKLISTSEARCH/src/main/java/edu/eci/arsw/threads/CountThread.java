33b757a6fc828535d706fb3b5b88d6d87bb12c8c
David Herrera <damoya2000@gmail.com>
2020-08-13 12:46:02 -0500

    Parte1

---
+++ BLACKLISTSEARCH/BLACKLISTSEARCH/src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThread extends Thread {
+    private int inicio, fin;
+
+    public CountThread (int inicio, int fin){
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+    public void run(){
+        for(int i = inicio;i<= fin;i++){
+            System.out.println(i);
+        }
+    }
+
+}
